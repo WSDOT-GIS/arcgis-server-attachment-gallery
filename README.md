15802f05dcff7b92c427362ae24a5ab07ab550f6
JeffJacobson <JeffJacobson@users.noreply.github.com>
2015-06-11 14:22:05 -0700

    :octocat: Added files to repository

---
+++ README.md
@@ -0,0 +1,12 @@
+﻿ArcGIS Server Attachment Gallery
+================================
+
+An HTML page that displays the attachments of an ArcGIS Server Map/Feature Service feature in a [Blueimp Gallery] Carousel control.
+
+## License ##
+
+Unless otherwise noted below, all code is in the public domain (See http://unlicense.org).
+
+* The [Blueimp Gallery] component referenced by this project is licensed under the MIT License.
+
+[Blueimp Gallery]:http://blueimp.github.io/Gallery/
---
+++ index.js
@@ -0,0 +1,52 @@
+﻿/// <reference path="bower_components/blueimp-gallery/js/blueimp-gallery.js" />
+
+/*global blueimp*/
+
+(function () {
+	var featureUrlRe = /\/\w+Server\/\d+\/\d+/i;
+	var featureUrl = location.hash;
+	var attachmentsUrl;
+	var gallery;
+	if (featureUrl) {
+		// Remove the hash from beginning of string.
+		featureUrl = featureUrl.replace(/^#/, "");
+		// Test to see if URL is a feature URL.
+		if (featureUrlRe.test(featureUrl)) {
+			console.log([featureUrl, "is a feature url"].join(" "));
+
+			if (!/\/attachments\/?/i.test(featureUrl)) {
+				attachmentsUrl = featureUrl.replace(/\/?$/, "/attachments");
+			}
+		}
+	}
+
+	var setupGallery = function (e) {
+		var request = e.target || e.currentTarget;
+		var responseUrl = request.responseURL.replace(/\?.+$/, ""); // remove query string / search.
+		var galleryData = JSON.parse(request.responseText);
+		console.log(galleryData);
+
+		if (galleryData.attachmentInfos) {
+			galleryData = galleryData.attachmentInfos.map(function (item) {
+				var attachmentUrl = [responseUrl, item.id].join("/");
+				return {
+					title: item.name,
+					href: attachmentUrl,
+					type: item.contentType || null,
+					thumbnail: attachmentUrl
+				};
+			});
+
+			gallery = blueimp.Gallery(galleryData);
+		}
+	};
+
+	var request;
+	if (attachmentsUrl) {
+		request = new XMLHttpRequest();
+		request.open("get", attachmentsUrl + "?f=json");
+		request.addEventListener("loadend", setupGallery);
+		request.send();
+	}
+
+}());
