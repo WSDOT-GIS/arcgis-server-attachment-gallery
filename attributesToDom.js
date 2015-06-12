cd1bdeffbd6752a9a25da55cffd3e974b904ef46
JeffJacobson <JeffJacobson@users.noreply.github.com>
2015-06-11 18:00:50 -0700

    Added feature attributes table

---
+++ attributesToDom.js
@@ -0,0 +1,112 @@
+﻿
+/*global define, module*/
+
+(function (root, factory) {
+	if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module.
+		define([], factory);
+	} else if (typeof exports === 'object') {
+		// Node. Does not work with strict CommonJS, but
+		// only CommonJS-like environments that support module.exports,
+		// like Node.
+		module.exports = factory();
+	} else {
+		// Browser globals (root is window)
+		root.attributesToDom = factory();
+	}
+}(this, function () {
+
+	var dateTimeFormatOptions = {
+		year: 'numeric', month: 'numeric', day: 'numeric',
+		timeZone: "UTC" // "America/Los_Angeles"
+	};
+	var dateTimeFormat = this.Intl ? new this.Intl.DateTimeFormat("en-us", dateTimeFormatOptions) : null;
+
+
+	function getTimeElement(date) {
+		var time;
+		var dateString = dateTimeFormat ? dateTimeFormat.format(date) : String(date);
+		try {
+			time = document.createElement("time");
+		} catch (e) {
+			time = null;
+		}
+		if (time) {
+			time.setAttribute("dateTime", dateString);
+			time.textContent = dateString;
+		} else {
+			time = document.createTextNode(dateString);
+		}
+		return time;
+	}
+
+	var exports = {
+		/**
+		 * Converts feature attributes object into a table.
+		 * @param {Object} attributes
+		 * @returns {HTMLTableElement}
+		 */
+		objectToTable: function (attributes) {
+			var table = document.createElement("table");
+			var row, cell, value;
+
+			for (var name in attributes) {
+				if (attributes.hasOwnProperty(name)) {
+					value = attributes[name];
+
+					row = document.createElement("tr");
+					cell = document.createElement("th");
+					cell.textContent = name;
+					row.appendChild(cell);
+
+					cell = document.createElement("td");
+
+					if (value instanceof Date) {
+						cell.appendChild(getTimeElement(value));
+					} else {
+						cell.textContent = value;
+
+					}
+					row.appendChild(cell);
+
+
+					table.appendChild(row);
+				}
+			}
+			return table;
+		},
+
+		/**
+		 * Converts feature attributes object into a definition list.
+		 * @param {Object} attributes
+		 * @returns {HTMLDListElement}
+		 */
+		objectToDL: function (attributes) {
+			var dl, dt, dd, value;
+
+			dl = document.createElement("dl");
+
+			for (var name in attributes) {
+				if (attributes.hasOwnProperty(name)) {
+					value = attributes[name];
+
+					dt = document.createElement("dt");
+					dt.textContent = name;
+					dl.appendChild(dt);
+
+					dd = document.createElement("dd");
+					dd.textContent = value;
+					dl.appendChild(dd);
+				}
+
+			}
+
+			return dl;
+		}
+	};
+
+	// Just return a value to define the module export.
+	// This example returns an object, but the module
+	// can return a function as the exported value.
+	return exports;
+}));
--- index.js
+++ index.js
@@ -1,10 +1,133 @@
 ﻿/// <reference path="bower_components/blueimp-gallery/js/blueimp-gallery.js" />
+/// <reference path="C:\Users\jacobsj\Documents\GitHub\arcgis-server-attachment-gallery\attributesToDom.js" />
 
-/*global blueimp*/
+/*global blueimp, attributesToDom*/
+
+/**
+ * @external AttachmentInfos
+ * @see {@link http://resources.arcgis.com/en/help/arcgis-rest-api/index.html#/Attachment_Infos_Map_Service_Layer/02r3000000r0000000/ Attachment Infos (Map Service\Layer)}
+ */
 
 (function () {
+	/**
+	 * 
+	 * @returns {Promise}
+	 */
+	function getAttachmentInfo() {
+		var promise = new Promise(function (resolve, reject) {
+			request = new XMLHttpRequest();
+			request.open("get", attachmentsUrl + "?f=json");
+			request.addEventListener("loadend", function (e) {
+				var request, galleryData;
+				request = e.target || e.currentTarget;
+				if (request.status === 200) {
+					galleryData = JSON.parse(request.responseText);
+					resolve(galleryData.attachmentInfos);
+				} else {
+					reject(e);
+				}
+			});
+			request.send();
+
+		});
+
+		return promise;
+	}
+
+	/**
+	 * 
+	 * @returns {Promise}
+	 */
+	function getFeatureInfo() {
+		var promise = new Promise(function (resolve, reject) {
+			request = new XMLHttpRequest();
+			request.open("get", featureUrl + "?f=json");
+			request.addEventListener("loadend", function (e) {
+				var request, layerInfo;
+				request = e.target || e.currentTarget;
+				if (request.status === 200) {
+					layerInfo = JSON.parse(request.responseText);
+					if (layerInfo.error) {
+						reject(layerInfo.error);
+					} else {
+						resolve(layerInfo);
+					}
+				} else {
+					reject(e);
+				}
+			});
+			request.send();
+
+		});
+
+		return promise;
+	}
+
+	/**
+	 * 
+	 * @returns {Promise}
+	 */
+	function getLayerInfo() {
+		var promise = new Promise(function (resolve, reject) {
+			request = new XMLHttpRequest();
+			request.open("get", layerUrl + "?f=json");
+			request.addEventListener("loadend", function (e) {
+				var request, layerInfo;
+				request = e.target || e.currentTarget;
+				if (request.status === 200) {
+					layerInfo = JSON.parse(request.responseText);
+					if (layerInfo.error) {
+						reject(layerInfo.error);
+					} else {
+						resolve(layerInfo);
+					}
+				} else {
+					reject(e);
+				}
+			});
+			request.send();
+
+		});
+
+		return promise;
+	}
+
+	function getAttributes() {
+		var promise = new Promise(function (resolve, reject) {
+			Promise.all([getLayerInfo(), getFeatureInfo()]).then(function (values) {
+				// Omit OBJECTID and Shape fields.
+				var ignoredFieldsRe = /^(?:(?:OBJECTID(?:_\d+)?)|(?:Shape(?:\.STLength\(\))?))$/i;
+				var dateRe = /^esriFieldTypeDate$/i;
+				var layerInfo = values[0];
+				var attributes = values[1].feature.attributes;
+				var fields = layerInfo.fields;
+				//var displayField = layerInfo.displayField;
+
+				var output = {};
+
+				fields.forEach(function (field) {
+					var value;
+					if (!ignoredFieldsRe.test(field.name)) {
+						value = attributes[field.name];
+
+						if (dateRe.test(field.type)) {
+							value = new Date(value);
+						}
+
+						output[field.alias || field.name] = value;
+					}
+				});
+
+				resolve(output);
+			}, function (error) {
+				reject(error);
+			});
+		});
+		return promise;
+	}
 	var featureUrlRe = /\/\w+Server\/\d+\/\d+/i;
 	var featureUrl = location.hash;
+	var layerUrl;
 	var attachmentsUrl;
 	var gallery;
 	if (featureUrl) {
@@ -12,7 +135,7 @@
 		featureUrl = featureUrl.replace(/^#/, "");
 		// Test to see if URL is a feature URL.
 		if (featureUrlRe.test(featureUrl)) {
-			console.log([featureUrl, "is a feature url"].join(" "));
+			layerUrl = featureUrl.replace(/\/\d+\/?$/, "");
 
 			if (!/\/attachments\/?/i.test(featureUrl)) {
 				attachmentsUrl = featureUrl.replace(/\/?$/, "/attachments");
@@ -20,14 +143,25 @@
 		}
 	}
 
-	var setupGallery = function (e) {
-		var request = e.target || e.currentTarget;
-		var responseUrl = attachmentsUrl.replace(/\?.+$/, ""); // remove query string / search.
-		var galleryData = JSON.parse(request.responseText);
-		console.log(galleryData);
 
-		if (galleryData.attachmentInfos) {
-			galleryData = galleryData.attachmentInfos.map(function (item) {
+
+	var request;
+	if (attachmentsUrl) {
+
+		getAttributes().then(function (attributes) {
+			var table = attributesToDom.objectToTable(attributes);
+			table.classList.add("attributes");
+			var caption = document.createElement("caption");
+			caption.textContent = "Feature Attributes";
+			table.insertBefore(caption, table.firstChild);
+			document.getElementById("blueimp-gallery").appendChild(table);
+		}, function (error) {
+			console.error(error);
+		});
+
+		getAttachmentInfo().then(function (attachmentInfos) {
+			var responseUrl = attachmentsUrl.replace(/\?.+$/, ""); // remove query string / search.
+			var galleryData = attachmentInfos.map(function (item) {
 				var attachmentUrl = [responseUrl, item.id].join("/");
 				return {
 					title: item.name,
@@ -37,16 +171,10 @@
 				};
 			});
 
-			gallery = blueimp.Gallery(galleryData);
-		}
-	};
-
-	var request;
-	if (attachmentsUrl) {
-		request = new XMLHttpRequest();
-		request.open("get", attachmentsUrl + "?f=json");
-		request.addEventListener("loadend", setupGallery);
-		request.send();
+			gallery = blueimp.Gallery(galleryData, {
+				carousel: true
+			});
+		});
 	}
 
 }());
